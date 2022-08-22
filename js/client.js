531be70ea39c4b52174eeeba191413497d17c24c
Puru-Raj <raj.16@iitj.ac.in>
2022-08-22 23:20:18 +0530

    initial

--- js/client.js
+++ js/client.js
@@ -1,10 +1,15 @@
 const socket=io('http://localhost:8000');
 // var CryptoJS = require("crypto-js");
 
+//get DOM elements in respective JS variables
 const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
+
+//audio will play on receiving message
 var audio = new Audio('../notification.mp3');
+
+//function which will append event to the container
 const append = (message, position) => {
     const messageElement = document.createElement("div"); 
     messageElement.innerText = message;
@@ -16,16 +21,28 @@ const append = (message, position) => {
 }
 }
 
+//Ask new User for his name and let the server know
 const myname=prompt("Enter your name to join");
 socket.emit('new-user-joined',myname);//emit the event new-user-joined and pass the name of the user
 
-
-//listen to the event receive from the server and pass the message and name of the user
+//if a new user joins receive his name from server
 socket.on('user-joined',myname=>{
     append(`${myname} joined the chat`, "right");
 }
 );
 
+//if server sends a message receive it
+socket.on('receive',data=>{
+    append(`${data.myname}:${data.message}`, "left");
+}
+);
+
+//if a user leaves the chat ,append the info to container
+socket.on('left',myname=>{
+    append(`${myname} left the chat`, "left");
+})
+
+//if the form gets submitted,send server the message
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const message=messageInput.value;
@@ -36,12 +53,6 @@ form.addEventListener('submit',(e)=>{
     messageInput.value  ="";
 })
 
-socket.on('receive',data=>{
-    append(`${data.myname}:${data.message}`, "left");
-}
-);
 
-socket.on('left',myname=>{
-    append(`${myname} left the chat`, "left");
-})
+
 
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -4,19 +4,24 @@ const io=require('socket.io')(8000);
 const users={};
 
 io.on('connection',socket=>{
+    //new-user-joined is the event name which is emitted from client.js 
     socket.on('new-user-joined',myname=>{ 
-        //new-user-joined is the event name which is emitted from client.js 
+        
         //console.log('new user joined',myname);
         socket.broadcast.emit('user-joined',myname);//broadcast the event user-joined and pass the name of the user
         users[socket.id]=myname;
         
     });
+
+    //if someone sends a message broadcast it to other users
     socket.on('send',message=>{
         // var bytes  = CryptoJS.AES.decrypt(message, 'secret key 123');
         // var originalText = bytes.toString(CryptoJS.enc.Utf8);
         socket.broadcast.emit('receive',{message:message,myname:users[socket.id]});
     });
 
+    //if someone leaves the chat, let others know it
+    //disconnnect is a built-in event which gets fired when user leaves
     socket.on('disconnect',()=>{
         socket.broadcast.emit('left',users[socket.id]);
         delete users[socket.id];
